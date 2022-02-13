9298869dc384d384cdb1cac45845c61ef5f1f516
Malachy O'Connor <malachyguitar@gmail.com>
2022-02-13 18:53:15 +0000

    Added image buttons

---
+++ src/renderer/pages/OnOpenPage.vue
@@ -0,0 +1,29 @@
+<template>
+
+
+  <h1 class="page-title" style="margin-top: 10%;">VIDEO BONES</h1>
+  <menu class="horizontal-options-menu" style="margin-top: 50px">
+    <div>
+      <img src="../../../assets/images/folderIcon.png">
+      <h3 class="page-title">OPEN FILE</h3>
+    </div>
+
+    <div>
+      <img src="../../../assets/images/addIcon.png">
+      <h3>ADD ELEMENT</h3>
+    </div>
+    
+  </menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "OnOpenPage",
+});
+</script>
+
+<style src="../styles/main.scss">
+</style>
+
