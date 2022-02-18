deaa42c6b833f55938becd0d9967282ea1b71136
Pablo Lanza <pl486@cam.ac.uk>
2022-02-18 13:12:07 +0000

    RecordingScreen nearly done

---
+++ src/renderer/pages/RecordingScreen.vue
@@ -0,0 +1,143 @@
+<template>
+  <video ref=videoPreview> </video>
+
+  <div class="horizontal-spacer">
+    <select class=dropdown ref=audioDevices @change="onAudioChange($event)"> </select>
+
+    <button class="image-container">
+        
+    </button>
+
+    <select class=dropdown ref=videoDevices @change="onVideoChange($event)"> </select>
+  </div>
+</template>
+
+<script lang="ts">
+import path from 'path'
+import * as fs from 'fs'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "RecordingScreen",
+  data() {
+    return {
+      videoDevice: "default",
+      audioDevice: "default",
+      videoRecorder: new MediaRecorder(new MediaStream(), undefined),
+      audioRecorder: new MediaRecorder(new MediaStream(), undefined),
+      videoChunks: <Blob[]> [],
+      audioChunks: <Blob[]> [],
+    }
+  },
+  methods: {
+    onAudioChange(event: Event) {
+      this.audioDevice = (<HTMLSelectElement> event.target).value;
+      this.startStreams()
+    },
+    onVideoChange(event: Event) {
+      this.videoDevice = (<HTMLSelectElement> event.target).value
+      this.startStreams()
+    },
+    startStreams() {
+      const video = <HTMLVideoElement> this.$refs.videoPreview;
+      const videoConstraints = {
+        audio: false,
+        video: { width: 1280, height: 720, deviceId: this.videoDevice }
+      }
+      // Get video stream
+      navigator.mediaDevices.getUserMedia(videoConstraints)
+        .then(stream => {
+          video.srcObject = stream;
+
+          // mp4/mpeg not supported by electron, though YouTube does support .webm
+          // Alternatively we can use ffmpeg to convert between file formats
+          const options = { mimeType: 'video/webm' };
+          this.videoRecorder = new MediaRecorder(stream, options);
+
+          // Set mediaRecorder funcionality
+          const that = this;
+          this.videoRecorder.ondataavailable = function(event) {
+            that.handleDataAvailable(event, 'video');
+          };
+          this.videoRecorder.onstop = this.download;
+        })
+
+      const audioConstraints = {
+        audio: { deviceId: this.audioDevice },
+        video: false
+      }
+      // Get audio stream
+      navigator.mediaDevices.getUserMedia(audioConstraints)
+        .then(stream => {
+          const options = { mimeType: 'audio/webm' };
+          this.audioRecorder = new MediaRecorder(stream, options);
+
+          // Set audioRecorder funcionality
+          const that = this;
+          this.audioRecorder.ondataavailable = function(event) {
+            that.handleDataAvailable(event, 'audio');
+          }
+          this.audioRecorder.onstop = this.download
+        })
+    },
+    handleDataAvailable(event: BlobEvent, type: string) {
+      if (event.data.size > 0) {
+        if (type === 'video') {
+          this.videoChunks.push(event.data)
+        } else if (type === 'audio') {
+          this.audioChunks.push(event.data)
+        }
+      }
+    },
+    download() {
+      const videoBlob = new Blob(this.videoChunks, { type: 'video/webm' })
+      videoBlob.arrayBuffer().then(buffer => {
+        const videoData = Buffer.from(buffer)
+        const videoPath = path.join('media', 'video.webm')
+        fs.writeFile(videoPath, videoData, err => {
+          if (err) throw err
+        })
+      })
+
+      const audioBlob = new Blob(this.audioChunks)
+      audioBlob.arrayBuffer().then(buffer => {
+        const audioData = Buffer.from(buffer)
+        const audioPath = path.join('media', 'audio.wav')
+        fs.writeFile(audioPath, audioData, err => {
+          if (err) throw err
+        })
+      })
+    },
+  },
+  mounted () {
+    const video = <HTMLVideoElement> this.$refs.videoPreview;
+    const audioDevices = <HTMLSelectElement> this.$refs.audioDevices
+    const videoDevices = <HTMLSelectElement> this.$refs.videoDevices
+
+    // Adds devices onto audio and video input device menus
+    navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+        devices.forEach(device => {
+          if (device.kind.toString() === 'audioinput') {
+            if (device.deviceId !== 'default' && device.deviceId !== 'communications') {
+              console.log(device.label)
+              audioDevices.options[audioDevices.options.length] = new Option(device.label, device.deviceId);
+            }
+          } else if (device.kind.toString() === 'videoinput') {
+            if (device.deviceId !== 'default') {
+              videoDevices.options[videoDevices.options.length] = new Option(device.label, device.deviceId);
+            }
+          }
+        })
+      })
+
+    video.autoplay = true;
+    this.startStreams()
+  }
+})
+</script>
+
+<style lang="scss">
+  @import "../styles/main.scss";
+  @import "../styles/pages/recording-page.scss"
+</style>
