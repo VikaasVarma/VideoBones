b9ff2f35e786fc64d47b80b61bb3ca637acf808a
Malachy O'Connor <malachyguitar@gmail.com>
2022-02-23 16:37:05 +0000

    Fixed single video editor styles

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -28,7 +28,7 @@ export default defineComponent({
         dynamicComponent = "CreateNewProjectPage" 
 
       }
-      return {"dynamicComponent":OnOpenPage}
+      return {"dynamicComponent":SingleVideoEditorPage}
 
       // return {dynamicComponent, onClick, 
               // "OnOpenPage" : OnOpenPage,
