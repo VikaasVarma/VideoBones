c19120dbda7b83ff23176d84edbe1da1efa49bef
Malachy O'Connor <malachyguitar@gmail.com>
2022-02-28 12:53:15 +0000

    Re-added Vikaas's video editor page

--- src/renderer/pages/App.vue
+++ src/renderer/pages/App.vue
@@ -2,6 +2,7 @@
   <create-new-project-page v-if="pageDisplayed === 'create-new-project-page'"/>
   <on-open-page @update="pageDisplayed = 'create-new-project-page'" v-else-if="pageDisplayed === 'on-open-page'"/>
   <single-video-editor-page v-else-if="pageDisplayed === 'single-video-editor-page'"/>
+  <video-editor-page v-else-if="pageDisplayed === 'video-editor-page'"/>
 </template>
 
 <script lang="ts">
@@ -9,13 +10,15 @@ import { defineComponent, ref } from 'vue'
 import OnOpenPage from './OnOpenPage.vue'
 import CreateNewProjectPage from './CreateNewProjectPage.vue'
 import SingleVideoEditorPage from './SingleVideoEditorPage.vue'
+import VideoEditorPage from './VideoEditorPage.vue';
 
 export default defineComponent({
     name: "app",
     components : {
       OnOpenPage,
       CreateNewProjectPage,
-      SingleVideoEditorPage 
+      SingleVideoEditorPage,
+      VideoEditorPage, 
     },
     // props : {
     //   pageDisplayed : {type:String, default: "on-open-page"},
@@ -27,7 +30,7 @@ export default defineComponent({
       }
     },
     setup(props, context) {
-      var pageDisplayed = ref("on-open-page")
+      var pageDisplayed = ref("video-editor-page")
       
       function onClick() {
         pageDisplayed.value = "create-new-project-page"
