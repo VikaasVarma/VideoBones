4e41b0aecfa0b046e0b13ffb3fa0057a5b7faf54
Vikaas Varma <vikaas.varma@gmail.com>
2022-02-28 23:15:38 +0000

    Refined design for On Open Page and Create New Project

--- src/renderer/pages/OnOpenPage.vue
+++ src/renderer/pages/OnOpenPage.vue
@@ -1,27 +1,25 @@
 <template id="OnOpenPage">
-  <div class="base-page-div">
     <h1 class="page-title" style="margin-top: 10%; margin-bottom: 5%;">VIDEO BONES</h1>
     <menu class="horizontal-options-menu">
 
-      <div>
-        <button @click="folderIconClicked()" class="image-container">
-          <img src="../../../assets/images/addIcon.png">
-        </button>
-        
-        <h3>Create New Project</h3>
-      </div>
+        <div>
+            <button @click="folderIconClicked()" class="image-container">
+                <img src="../../../assets/images/addIcon.png">
+            </button>
+            
+            <h3>Create New Project</h3>
+        </div>
 
-      <hr class="vertical-border-line">
+        <hr class="vertical-border-line">
 
-      <div>
-        <button class="image-container">
-          <img src="../../../assets/images/folderIcon.png">
-        </button>
+        <div>
+            <button class="image-container">
+                <img src="../../../assets/images/folderIcon.png">
+            </button>
 
-        <h3>Open Existing Project</h3>
-      </div>
+            <h3>Open Existing Project</h3>
+        </div>
     </menu>
-  </div>
 </template>
 
 <script lang="ts">
