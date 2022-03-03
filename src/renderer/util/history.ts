3ae349a97f0da66f4ea6db4ac28f9f6f9a6061a3
yeroCG <corey_gair@yahoo.co.uk>
2022-03-03 14:51:11 +0000

    Changed privates to #

--- src/renderer/util/history.ts
+++ src/renderer/util/history.ts
@@ -22,14 +22,14 @@ function createOperation(execute:()=>void, undo:()=>void): Operation {
  * Has a max size, which once exceeded drops elements from the bottom of the stack.
  */
 export class OperationUndoStack {
-  private top: Operation | null = null;
-  private bottom: Operation | null = null;
+  #top: Operation | null = null;
+  #bottom: Operation | null = null;
 
-  private size = 0;
-  private maxSize: number;
+  #size = 0;
+  #maxSize: number;
 
   constructor(maxSize:number) {
-    this.maxSize = maxSize;
+    this.#maxSize = maxSize;
   }
 
   /**
@@ -42,29 +42,29 @@ export class OperationUndoStack {
     const operation = createOperation(execute, undo);
 
     if (this.isEmpty()) {
-      this.top = operation;
-      this.bottom = operation;
-      ++this.size;
+      this.#top = operation;
+      this.#bottom = operation;
+      ++this.#size;
     } else if (this.isFull()) {
       // stack is full, drop the bottom (least recently executed)
-      this.bottom = (this.bottom as Operation).next;
-      (this.bottom as Operation).prev = null;
+      this.#bottom = (this.#bottom as Operation).next;
+      (this.#bottom as Operation).prev = null;
 
-      operation.prev = this.top;
-      (this.top as Operation).next = operation;
-      this.top = operation;
+      operation.prev = this.#top;
+      (this.#top as Operation).next = operation;
+      this.#top = operation;
     } else {
-      operation.prev = this.top;
-      (this.top as Operation).next = operation;
-      this.top = operation;
-      ++this.size;
+      operation.prev = this.#top;
+      (this.#top as Operation).next = operation;
+      this.#top = operation;
+      ++this.#size;
     }
 
     execute();
   }
 
   /**
-   * Undoes the top operation of the stack.
+   * Undoes the #top operation of the stack.
    * 
    * Throws error if stack empty.
    */
@@ -73,11 +73,11 @@ export class OperationUndoStack {
       throw Error('Nothing to undo!');
     }
 
-    (this.top as Operation).undo();
+    (this.#top as Operation).undo();
 
-    this.top = (this.top as Operation).prev;
+    this.#top = (this.#top as Operation).prev;
 
-    --this.size;
+    --this.#size;
   }
 
   /**
@@ -87,44 +87,44 @@ export class OperationUndoStack {
    */
   redo() {
     if (this.isEmpty()) {
-      if (this.bottom !== null) {
-        this.top = this.bottom;
-        this.top.execute();
-        ++this.size;
+      if (this.#bottom !== null) {
+        this.#top = this.#bottom;
+        this.#top.execute();
+        ++this.#size;
         return;
       }
 
       throw Error('Nothing to redo!');
     }
 
-    if (this.isFull() || (this.top as Operation).next === null) {
+    if (this.isFull() || (this.#top as Operation).next === null) {
       throw Error('Nothing to redo!');
     }
 
-    this.top = (this.top as Operation).next;
-    (this.top as Operation).execute();
-    ++this.size;
+    this.#top = (this.#top as Operation).next;
+    (this.#top as Operation).execute();
+    ++this.#size;
   }
 
   isEmpty(): boolean {
-    return this.size === 0;
+    return this.#size === 0;
   }
 
   isFull():boolean {
-    return this.size === this.maxSize;
+    return this.#size === this.#maxSize;
   }
 
   /**
    * Will undo() succeed?
    */
   canUndo(): boolean {
-    return top !== null;
+    return this.#top !== null;
   }
 
   /**
    * Will redo() succeed?
    */
   canRedo(): boolean {
-    return !this.isFull() && !(this.isEmpty() && this.bottom === null);
+    return !this.isFull() && !(this.isEmpty() && this.#bottom === null);
   }
 }
