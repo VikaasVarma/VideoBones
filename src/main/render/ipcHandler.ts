572ed49bc1769c4a0e6e588c1028ead8d07fb1ca
Malachy O'Connor <malachyguitar@gmail.com>
2022-03-06 13:46:18 +0000

    Update ipcHandler.ts
    dsadsa

--- src/main/render/ipcHandler.ts
+++ src/main/render/ipcHandler.ts
@@ -1,7 +1,7 @@
 import { kill, start } from './engine';
 import { ipcMain } from 'electron';
 
-export function startHandler() {
+export function startHandler(port:number) {
   ipcMain.addListener('asynchronous-message', (event, arg) => {
     if (!arg.type) {
       console.warn('Incorrect type from IPC', arg);
@@ -11,7 +11,7 @@ export function startHandler() {
     switch (arg.type) {
         case 'startEngine':
           start(arg.data, (elapsedTime, donePercentage) => {
-            event.sender.send('asynchronous-reply', { event: 'progress', elapsedTime, donePercentage });
+            event.sender.send('asynchronous-reply', { event: 'progress', elapsedTime, donePercentage, port });
           }, () => {
             event.sender.send('asynchronous-reply', { event: 'done' });
           });
