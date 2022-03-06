9f262a300cafc4acbe91e3c65fcc307de1ad5fb0
DaniFoldi <dani@stefan.hu>
2022-03-06 13:20:49 +0000

    Remove test metronome

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -3,10 +3,6 @@ import './styles/reset.scss';
 import App from './pages/App.vue';
 import { createApp } from 'vue';
 
-import { generateMetronome } from './util/metronome';
-
 const app = createApp(App);
 
 app.mount('#app');
-
-generateMetronome({ bpm: 120, length: 10 });
