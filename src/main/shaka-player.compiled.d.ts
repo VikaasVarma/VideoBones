5e783dda7d270f133d1c445477ea72be100a0739
DaniFoldi <dani@stefan.hu>
2022-03-08 20:30:41 +0000

    Fix more unlikely-to-cause-conflicts linting errors

--- src/main/shaka-player.compiled.d.ts
+++ src/main/shaka-player.compiled.d.ts
@@ -1,8 +1,11 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-undef */
 declare module 'shaka-player' {
+  // @ts-ignore
   export = shaka;
 }
 
 declare module 'shaka-player/dist/shaka-player.compiled' {
+  // @ts-ignore
   export = shaka;
 }
