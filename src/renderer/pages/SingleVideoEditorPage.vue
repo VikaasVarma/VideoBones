70308b2f8fab6ae84266a1d97383e23e06142bbd
Malachy O'Connor <malachyguitar@gmail.com>
2022-03-09 10:02:30 +0000

    Slider values now update automagically

--- src/renderer/pages/SingleVideoEditorPage.vue
+++ src/renderer/pages/SingleVideoEditorPage.vue
@@ -23,26 +23,27 @@
             </div>
 
 
-            <menu class="vertical-options-menu">
+            <menu @click="test()" class="vertical-options-menu">
                 <div>
                     <h3  class="section-title">Audio Effects</h3 >
                     
                     <tickbox-component @click="reverb_enabled=!reverb_enabled" tickbox_text="Enable Reverb" />
 
-                    <slider-component  v-if="reverb_enabled" slider_name="Delay" />
-                    <slider-component v-if="reverb_enabled" slider_name="Decay" />
+                    <slider-component  v-model:slider_value="reverb_settings.delay"
+                                        v-if="reverb_enabled" slider_name="Delay" />
+
+                    <slider-component v-model:slider_value="reverb_settings.decay"
+                                        v-if="reverb_enabled" slider_name="Decay" />
 
                     <tickbox-component @click="echo_enabled=!echo_enabled" tickbox_text="Enable Echo"/>
 
-                    <slider-component v-if="echo_enabled" slider_name="Delay" />
-                    <slider-component v-if="echo_enabled" slider_name="Decay" />
+                    <slider-component v-model:slider_value="echo_settings.delay"
+                                     v-if="echo_enabled" slider_name="Delay" />
+                    <slider-component v-model:slider_value="echo_settings.decay" 
+                                       v-if="echo_enabled" slider_name="Decay" />
 
                     <tickbox-component @click="denoise_enabled=!denoise_enabled" tickbox_text="Denoise"/>
 
-                </div>
-
-                <div>
-                    <h2 class="section-title">Rerecord Sections</h2>
                 </div>
 
             </menu>
@@ -67,10 +68,16 @@ export default defineComponent({
             reverb_enabled : false, 
             echo_enabled : false,
             denoise_enabled : false,
-            reverb_settings : {},
-            
+            reverb_settings : {decay: 0, delay: 0},
+            echo_settings : {decay: 0, delay: 0},
+
         }
-    }
+    },
+    methods : {
+        test () {
+            console.log(this.reverb_settings, this.echo_settings)
+        }
+    },
     // djsjahkdsa.send("reverb-settings-changed", {decay:3728, delay:3278327})
     // djsjahkdsa.send("echo-settings-changed", {decay:3728, delay:3278327})
 });
