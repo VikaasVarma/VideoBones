c80530cde55830cf501995d4ba61b11e09cb4647
yeroCG <corey_gair@yahoo.co.uk>
2022-03-09 17:01:45 +0000

    Preview now appears much faster when opening editor

--- src/renderer/components/VideoPlayer.vue
+++ src/renderer/components/VideoPlayer.vue
@@ -42,9 +42,6 @@ export default defineComponent({
     player
       .load(this.manifestUrl)
       .catch(this.onError);
-    
-    // reduce the buffering amt to hopefully speed up preview starting
-    player.configure('streaming.bufferingGoal', 2);
   },
   methods: {
     onError(error: Error) {
