df4c0ac8cdb858ef10aef42fd7acb90e9c58e6cb
DaniFoldi <dani@stefan.hu>
2022-03-12 11:13:24 +0000

    Fix VideoPlayer

--- src/renderer/components/VideoPlayer.vue
+++ src/renderer/components/VideoPlayer.vue
@@ -5,13 +5,14 @@
       ref="videoPlayer"
       class="w-full h-full"
       :poster="posterUrl"
-    ></video>
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 import * as shaka from 'shaka-player';
+
 
 export default defineComponent({
   props: {
@@ -26,17 +27,17 @@ export default defineComponent({
     }
   },
   mounted() {
-    const player = new shaka.Player(this.$refs.videoPlayer as HTMLMediaElement);
+    const player = new shaka.Player(this.$refs.videoPlayer as HTMLVideoElement);
     player.configure({
-      streaming: {
-        lowLatencyMode: true,
-        inaccurateManifestTolerance: 0,
-        rebufferingGoal: 0.01,
-      },
-      manifest:{
-        dash:{
-          ignoreMinBufferTime: true,
+      manifest: {
+        dash: {
+          ignoreMinBufferTime: true
         }
+      },
+      streaming: {
+        inaccurateManifestTolerance: 0,
+        lowLatencyMode: true,
+        rebufferingGoal: 0.01
       }
     });
     player
@@ -44,28 +45,28 @@ export default defineComponent({
       .catch(this.onError);
   },
   methods: {
+    getCurrentTime() {
+      return (this.$refs.videoPlayer as HTMLVideoElement).currentTime;
+    },
+    getEndTime() {
+      return (this.$refs.videoPlayer as HTMLVideoElement).duration;
+    },
     onError(error: Error) {
       console.error('Error code', error);
     },
-    getCurrentTime() {
-      return (<HTMLMediaElement>this.$refs.videoPlayer).currentTime;
-    },
-    getEndTime() {
-      return (<HTMLMediaElement>this.$refs.videoPlayer).duration;
-    },
-    seekToTime(newTime: number) {
-      if (newTime < 0 || newTime > (<HTMLMediaElement>this.$refs.videoPlayer).duration) {
-        throw Error(`Cannot seek to time ${newTime} in video of duration ${(<HTMLMediaElement>this.$refs.videoPlayer).duration}s.`)
-      }
-
-      (<HTMLMediaElement>this.$refs.videoPlayer).currentTime = newTime;
-    },
     pausePlayback() {
-      (<HTMLMediaElement>this.$refs.videoPlayer).pause();
+      (this.$refs.videoPlayer as HTMLVideoElement).pause();
     },
     resumePlayback() {
-      (<HTMLMediaElement>this.$refs.videoPlayer).play();
-    }
+      (this.$refs.videoPlayer as HTMLVideoElement).play();
+    },
+    seekToTime(newTime: number) {
+      if (newTime < 0 || newTime > (this.$refs.videoPlayer as HTMLVideoElement).duration) {
+        throw new Error(`Cannot seek to time ${newTime} in video of duration ${(this.$refs.videoPlayer as HTMLVideoElement).duration}s.`);
+      }
+
+      (this.$refs.videoPlayer as HTMLVideoElement).currentTime = newTime;
+    },
   }
 });
 </script>
